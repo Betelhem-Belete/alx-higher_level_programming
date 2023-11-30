6c7504f7e45c2554f41c1aac222d0a6fda47bef3
Betelhem <119102991+Betelhem-Belete@users.noreply.github.com>
2023-11-30 04:24:55 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for l in range(97, 123):
-    if l != 101 and l != 113:
-        print("{:c}".format(l), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
